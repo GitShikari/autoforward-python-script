48b43ad87cb9be325369cb9345fa4aa3d6733294
Mohammad Faizan Ahmad <pro327644@gmail.com>
2025-08-04 21:53:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 CHAT_ID = int(os.environ.get("CHAT_ID"))
 PER_USER = os.environ.get("PER_USER")
+SESSION_STRING =os.environ.get("SESSION")
 
-app = pyrogram.Client("my_account",api_id=API_ID,api_hash=API_HASH)
+app = pyrogram.Client("my_account",api_id=API_ID,api_hash=API_HASH,session_string=SESSION_STRING)
 app.start()
 app.send_message("me", "Greetings from **Pyrogram**!")
 id=0
