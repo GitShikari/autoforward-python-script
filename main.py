2a65e21545e93768edd7f8524c38b4edc486f5a0
Mohammad Faizan Ahmad <pro327644@gmail.com>
2025-08-05 16:31:56 +0530

    waits 10s before checking for newer message

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pyrogram, os
-# import dotenv
-# dotenv.load_dotenv()
+import dotenv
+dotenv.load_dotenv()
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 CHAT_ID = int(os.environ.get("CHAT_ID"))
 PER_USER = os.environ.get("PER_USER")
 SESSION_STRING =os.environ.get("SESSION")
+import time
 
 app = pyrogram.Client("my_account",api_id=API_ID,api_hash=API_HASH,session_string=SESSION_STRING,sleep_threshold=0)
 # app.start()
@@ -14,12 +15,13 @@ async def main():
     await app.send_message("me", "Greetings from **Pyrogram**!")
     id=0
     while True:
-      for b in await app.get_chat_history(chat_id=CHAT_ID,limit=1):
-        # print(b.id,b.text)
+      async for b in app.get_chat_history(chat_id=CHAT_ID,limit=1):
+        print(b.id,b.text)
         if id != b.id:
           latest_id = b.id
-          latest_text=b.text
+          # latest_text=b.text
           await app.copy_message(chat_id=PER_USER, from_chat_id=CHAT_ID, message_id=latest_id)
           id = latest_id
           print("Sent one message")
+      time.sleep(10)
 app.run(main())
