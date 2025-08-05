381fb4334b9fba01ee90590f4635b2fd0c575750
Mohammad Faizan Ahmad <pro327644@gmail.com>
2025-08-05 16:20:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ SESSION_STRING =os.environ.get("SESSION")
 
 app = pyrogram.Client("my_account",api_id=API_ID,api_hash=API_HASH,session_string=SESSION_STRING,sleep_threshold=0)
 # app.start()
-def main():
-  app.send_message("me", "Greetings from **Pyrogram**!")
-  id=0
-  while True:
-    for b in app.get_chat_history(chat_id=CHAT_ID,limit=1):
-      # print(b.id,b.text)
-      if id != b.id:
-        latest_id = b.id
-        latest_text=b.text
-        app.copy_message(chat_id=PER_USER, from_chat_id=CHAT_ID, message_id=latest_id)
-        id = latest_id
-        print("Sent one message")
+async def main():
+  async with app:
+    await app.send_message("me", "Greetings from **Pyrogram**!")
+    id=0
+    while True:
+      for b in await app.get_chat_history(chat_id=CHAT_ID,limit=1):
+        # print(b.id,b.text)
+        if id != b.id:
+          latest_id = b.id
+          latest_text=b.text
+          await app.copy_message(chat_id=PER_USER, from_chat_id=CHAT_ID, message_id=latest_id)
+          id = latest_id
+          print("Sent one message")
 app.run(main())
