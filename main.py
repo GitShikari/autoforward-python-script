38c0e02c73e8221b56f7d3c93ed45ae7380e1812
Mohammad Faizan Ahmad <pro327644@gmail.com>
2025-08-06 11:04:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,16 @@ CHAT_ID1 = int(os.environ.get("CHAT_ID1"))
 CHAT_ID2 = int(os.environ.get("CHAT_ID2"))
 PER_USER = os.environ.get("PER_USER")
 SESSION_STRING =os.environ.get("SESSION")
-import time
+# import time
 
 app = pyrogram.Client("my_account",api_id=API_ID,api_hash=API_HASH,session_string=SESSION_STRING,sleep_threshold=0)
 # app.start()
-async def main():
-  async with app:
-    await app.send_message("me", "Greetings from **Pyrogram**!")
-    id1, id2= 0, 0
-    while True:
-      #1
-      async for b in app.get_chat_history(chat_id=CHAT_ID1,limit=1):
-        print(b.id,b.text)
-        if id1 != b.id:
-          latest_id = b.id
-          # latest_text=b.text
-          await app.copy_message(chat_id=PER_USER, from_chat_id=CHAT_ID1, message_id=latest_id)
-          id1 = latest_id
-          print("Sent one message")
-      #2
-      async for b in app.get_chat_history(chat_id=CHAT_ID2,limit=1):
-        print(b.id,b.text)
-        if id2 != b.id:
-          latest_id = b.id
-          # latest_text=b.text
-          await app.copy_message(chat_id=PER_USER, from_chat_id=CHAT_ID2, message_id=latest_id)
-          id2 = latest_id
+
+@app.on_message(pyrogram.filters.chat(CHAT_ID1) | pyrogram.filters.chat(CHAT_ID2))
+async def message_handler(client,message):
+  print(message)
+  app.send_message(PER_USER,message.text)
+    
       
-      time.sleep(10)
-app.run(main())
+      # time.sleep(10)
+app.run()
